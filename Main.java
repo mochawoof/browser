d39e978a38dcd8b1fab0731a6fdf2dc222d0fb8b
Julian <julianherbert.2007@gmail.com>
2025-01-24 08:31:57 -0500

    a

--- Main.java
+++ Main.java
@@ -11,9 +11,75 @@ import javafx.beans.value.*;
 import javafx.event.*;
 import javafx.util.Callback;
 
+import java.util.ArrayList;
+
 class Main {
     private static JFrame frame;
     private static JPanel panel;
+    private static JTabbedPane tabs;
+    private static JTextField searchBar;
+    private static ArrayList<WebView> webs;
+    
+    private static void newTab() {
+        JFXPanel jfx = new JFXPanel();
+        tabs.add("Untitled", jfx);
+        int i = tabs.getTabCount();
+        
+        Platform.runLater(new Runnable() {
+            public void run() {
+                WebView web = new WebView();
+                webs.add(web);
+                WebEngine engine = web.getEngine();
+                
+                // JS events
+                engine.setOnAlert(new EventHandler<WebEvent<String>>() {
+                    public void handle(WebEvent<String> e) {
+                        JOptionPane.showMessageDialog(frame, e.getData(), "Message", JOptionPane.INFORMATION_MESSAGE);
+                    }
+                });
+                engine.setConfirmHandler(new Callback<String, Boolean>() {
+                    public Boolean call(String e) {
+                        return JOptionPane.showConfirmDialog(frame, e, "Confirm", JOptionPane.OK_CANCEL_OPTION) == JOptionPane.OK_OPTION;
+                    }
+                });
+                engine.setPromptHandler(new Callback<PromptData, String>() {
+                    public String call(PromptData e) {
+                        String s = (String) JOptionPane.showInputDialog(frame, e.getMessage(), "Input", JOptionPane.QUESTION_MESSAGE, null, null, e.getDefaultValue());
+                        return s;
+                    }
+                });
+                
+                // Change listeners
+                engine.titleProperty().addListener(new ChangeListener<String> () {
+                    public void changed(ObservableValue<?extends String> obs, String oldValue, String newValue) {
+                        SwingUtilities.invokeLater(new Runnable() {
+                            public void run() {
+                                String title = ((newValue == null || newValue.trim().equals("")) ? "Untitled" : " - " + newValue);
+                                //frame.setTitle("Browser" + ((newValue == null || newValue.trim().equals("")) ? "" : " - " + newValue));
+                                tabs.setTitleAt(i, title);
+                            }
+                        });
+                    }
+                });
+                engine.locationProperty().addListener(new ChangeListener<String> () {
+                    public void changed(ObservableValue<?extends String> obs, String oldValue, String newValue) {
+                        SwingUtilities.invokeLater(new Runnable() {
+                            public void run() {
+                                if (tabs.getSelectedIndex() == i) {
+                                    searchBar.setText(newValue);
+                                }
+                            }
+                        });
+                    }
+                });
+                
+                web.getEngine().load("https://testpages.herokuapp.com/styled/alerts/alert-test.html");
+                Scene scene = new Scene(new VBox(web));
+                jfx.setScene(scene);
+            }
+        });
+    }
+    
     public static void main(String[] args) {
         // Set theme
         // for (javax.swing.UIManager.LookAndFeelInfo l : javax.swing.UIManager.getInstalledLookAndFeels()) {System.out.println(l.getName());}
@@ -58,11 +124,16 @@ class Main {
         frame.add(panel, BorderLayout.CENTER);
         
         // Tabs
-        JTabbedPane tabs = new JTabbedPane();
+        tabs = new JTabbedPane();
         panel.add(tabs, BorderLayout.CENTER);
+        tabs.addChangeListener(new javax.swing.event.ChangeListener() {
+            public void stateChanged(javax.swing.event.ChangeEvent e) {
+                
+            }
+        });
         
         // Search bar
-        JTextField searchBar = new JTextField();
+        searchBar = new JTextField();
         panel.add(searchBar, BorderLayout.PAGE_START);
         searchBar.addActionListener(new ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent e) {
@@ -70,57 +141,8 @@ class Main {
             }
         });
         
-        JFXPanel jfx = new JFXPanel();
-        tabs.add("Hello", jfx);
-        
-        Platform.runLater(new Runnable() {
-            public void run() {
-                WebView web = new WebView();
-                WebEngine engine = web.getEngine();
-                
-                // JS events
-                engine.setOnAlert(new EventHandler<WebEvent<String>>() {
-                    public void handle(WebEvent<String> e) {
-                        JOptionPane.showMessageDialog(frame, e.getData(), "Message", JOptionPane.INFORMATION_MESSAGE);
-                    }
-                });
-                engine.setConfirmHandler(new Callback<String, Boolean>() {
-                    public Boolean call(String e) {
-                        return JOptionPane.showConfirmDialog(frame, e, "Confirm", JOptionPane.OK_CANCEL_OPTION) == JOptionPane.OK_OPTION;
-                    }
-                });
-                engine.setPromptHandler(new Callback<PromptData, String>() {
-                    public String call(PromptData e) {
-                        String s = (String) JOptionPane.showInputDialog(frame, e.getMessage(), "Input", JOptionPane.QUESTION_MESSAGE, null, null, e.getDefaultValue());
-                        return s;
-                    }
-                });
-                
-                // Change listeners
-                engine.titleProperty().addListener(new ChangeListener<String> () {
-                    public void changed(ObservableValue<?extends String> obs, String oldValue, String newValue) {
-                        SwingUtilities.invokeLater(new Runnable() {
-                            public void run() {
-                                frame.setTitle("Browser" + ((newValue == null || newValue.trim().equals("")) ? "" : " - " + newValue));
-                            }
-                        });
-                    }
-                });
-                engine.locationProperty().addListener(new ChangeListener<String> () {
-                    public void changed(ObservableValue<?extends String> obs, String oldValue, String newValue) {
-                        SwingUtilities.invokeLater(new Runnable() {
-                            public void run() {
-                                searchBar.setText(newValue);
-                            }
-                        });
-                    }
-                });
-                
-                web.getEngine().load("https://testpages.herokuapp.com/styled/alerts/alert-test.html");
-                Scene scene = new Scene(new VBox(web));
-                jfx.setScene(scene);
-            }
-        });
+        webs = new ArrayList<WebView>();
+        newTab();
 
         frame.setSize(800, 600);
         frame.setVisible(true);
